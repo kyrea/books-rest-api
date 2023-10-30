695b7c0a0975c344b5dafc8048d868b353934430
Aeryk <53209134+kyrea@users.noreply.github.com>
2023-10-30 17:23:47 +0530

    Fixed Counter not defined error

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,4 +1,5 @@
-const Book = require('../models/Books');
+const Book = require("../models/Books");
+const Counter = require("../models/Counter");
 
 // Controller functions for CRUD operations
 const bookController = {
@@ -8,8 +9,11 @@ const bookController = {
       const { title, author, summary } = req.body;
 
       // Fetch the current counter value and increment it
-      const counter = await Counter.findOneAndUpdate({ name: 'bookCount' }, { $inc: { value: 1 } });
-// Use the counter value as the custom _id for the new book
+      const counter = await Counter.findOneAndUpdate(
+        { name: "bookCount" },
+        { $inc: { value: 1 } }
+      );
+      // Use the counter value as the custom _id for the new book
       const newBook = new Book({
         _id: counter.value, // Custom _id
         title,
@@ -19,8 +23,8 @@ const bookController = {
       await newBook.save();
       res.status(201).json(book);
     } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 
@@ -30,8 +34,8 @@ const bookController = {
       const books = await Book.find();
       res.json(books);
     } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 
@@ -40,12 +44,12 @@ const bookController = {
     try {
       const book = await Book.findById(req.params.id);
       if (!book) {
-        return res.status(404).json({ error: 'Book not found' });
+        return res.status(404).json({ error: "Book not found" });
       }
       res.json(book);
     } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 
@@ -53,14 +57,18 @@ const bookController = {
   updateBook: async (req, res) => {
     try {
       const { title, author, summary } = req.body;
-      const book = await Book.findByIdAndUpdate(req.params.id, { title, author, summary }, { new: true });
+      const book = await Book.findByIdAndUpdate(
+        req.params.id,
+        { title, author, summary },
+        { new: true }
+      );
       if (!book) {
-        return res.status(404).json({ error: 'Book not found' });
+        return res.status(404).json({ error: "Book not found" });
       }
       res.json(book);
     } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 
@@ -69,12 +77,12 @@ const bookController = {
     try {
       const book = await Book.findByIdAndRemove(req.params.id);
       if (!book) {
-        return res.status(404).json({ error: 'Book not found' });
+        return res.status(404).json({ error: "Book not found" });
       }
-      res.json({ message: 'Book deleted successfully' });
+      res.json({ message: "Book deleted successfully" });
     } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 };
