39dd46be99cdf49c2257628327a31b9dfe2814b1
Aeryk <53209134+kyrea@users.noreply.github.com>
2023-10-30 16:54:30 +0530

    Added a fix to findOne() bug

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const bookRoutes = require('./routes/bookRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bookRoutes = require("./routes/bookRoutes");
 
 dotenv.config();
 const app = express();
@@ -12,19 +12,28 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-const Counter = require('./models/Counter');
+const Counter = require("./models/Counter");
 
 // Initialize the counter if it doesn't exist
-Counter.findOne({ name: 'bookCount' }, (err, counter) => {
-  if (!counter) {
-    const newCounter = new Counter({ name: 'bookCount', value: 1 });
-    newCounter.save();
-  }
-});
+async function updateCounter() {
+  try {
+    const counter = await Counter.findOne({ name: "bookCount" });
 
+    if (!counter) {
+      const newCounter = new Counter({ name: "bookCount", value: 1 });
+      await newCounter.save();
+    }
+  } catch (err) {
+    // Handle any errors that may occur during the database operations.
+    console.error(err);
+  }
+}
+
+// Call the async function to update the counter
+updateCounter();
 
 // Use routes
-app.use('/api/books', bookRoutes);
+app.use("/api/books", bookRoutes);
 
 // Start the server
 const port = process.env.PORT || 3000;
